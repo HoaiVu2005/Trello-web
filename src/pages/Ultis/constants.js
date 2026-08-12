8713e7a1cb1355c49eb1a28386296ab4d3967d5c
HoaiVu <lehoaivu.10092005@gmaill.com>
2026-08-12 21:59:00 +0700

    deploy v1

--- src/pages/Ultis/constants.js
+++ src/pages/Ultis/constants.js
@@ -4,7 +4,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8074'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-9oqp.onrender.com/'
+  apiRoot = 'https://trello-api-9oqp.onrender.com'
 }
 
 export const API_ROOT = apiRoot
