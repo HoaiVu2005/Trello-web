eeaa3b55ae0f3c1e5adb91202a2b5c84fed3a2a0
HoaiVu <lehoaivu.10092005@gmaill.com>
2026-08-12 20:07:22 +0700

    -up

--- src/pages/Ultis/constants.js
+++ src/pages/Ultis/constants.js
@@ -1,13 +1,12 @@
 let apiRoot = ''
-console.log('import.meta.env:', import.meta.env)
-console.log('process.env:', process.env)
+
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8074'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-9oqp.onrender.com'
+  apiRoot = 'https://trello-web-iouh.vercel.app/'
 }
-console.log('local:', apiRoot)
+
 export const API_ROOT = apiRoot
 // export const API_ROOT = 'http://localhost:8074'
 
